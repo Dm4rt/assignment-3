c3fa8d21e624638a1c3d6c51992f4313fd5bcc20
dm4rt <danmartinez2004@gmail.com>
2024-04-07 22:28:55 -0400

    added simple form to add new credits

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -5,11 +5,15 @@ The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
 
 const Credits = (props) => {
   console.log('Credit props:', props.credits);
   const {credits} = props; //We get the creditList from App.js
   console.log(credits);
+  const [description, updateDescription] = useState('');
+  const [amount, updateAmount] = useState(0);
+  
   const listSize = credits.length;
   const allCredits = [];
   //Iterate through the credit list and push to a new allCredits array we will display
@@ -26,10 +30,30 @@ const Credits = (props) => {
   	);
   }
   
+  const changeDescription = (event) =>{
+  	
+  };
+  
+  const changeAmount = (event) =>{
+  	
+  };
+  
+  const addCredit = () => {
+  	return;
+  }
+  
   return (
     <div>
       <h1>Credits</h1>
       <br/>
+      {/* add a form to take user input for new credits*/}
+      <h2>Add Credit: </h2>
+      <label htmlFor="description">Description</label>
+      <input type="text" id="description" value = {description} onChange= {changeDescription} />
+      <label htmlFor="amount">Amount</label>
+      <input type="number" id="amount" value = {amount} onChange= {changeAmount} />
+      <button onSubmit={addCredit}>Add</button>
+      
       <ul>{allCredits}</ul> 
       <Link to="/">Return to Home</Link>
     </div>
