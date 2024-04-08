c77b9cfd2f60118dcb38c7e9e51eb3f39272fa4b
dm4rt <danmartinez2004@gmail.com>
2024-04-07 21:24:05 -0400

    displayed basic creditList on Credits page

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -10,13 +10,26 @@ const Credits = (props) => {
   console.log('Credit props:', props.credits);
   const {credits} = props; //We get the creditList from App.js
   console.log(credits);
-  const testCredit = credits[0].id; //test credit
+  const listSize = credits.length;
+  const allCredits = [];
+  //Iterate through the credit list and push to a new allCredits array we will display
+  for(let i=0; i<listSize; i++){
+  	const curCredit = credits[i];
+  	allCredits.push(
+  		<li key={curCredit.id}>
+  		<h3>ID:</h3> {curCredit.id}, <br />
+  		<h3>Description:</h3> {curCredit.description}, <br />
+  		<h3>Amount:</h3> {curCredit.amount}, <br />
+  		<h3>Date:</h3> {curCredit.date}, <br />
+  		</li>
+  	);
+  }
   
   return (
     <div>
       <h1>Credits</h1>
       <br/>
-      <p>{testCredit}</p> 
+      <ul>{allCredits}</ul> 
       <Link to="/">Return to Home</Link>
     </div>
   );
